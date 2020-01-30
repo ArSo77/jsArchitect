e6f74dfd7a443df0e29fc4b54745c89ea12ba675
a.sobieraj <a.sobieraj@pixel.com.pl>
2020-01-30 09:45:29 +0100

    this update

--- js/base/context/this.js
+++ js/base/context/this.js
@@ -1,1 +1,12 @@
 // .call .apply .bind
+
+
+function theFunction(name, profession) {
+    console.log("My name is " + name + " and I am a " + profession +".");
+}
+// theFunction.apply(valueForThis, arrayOfArgs)
+// theFunction.call(valueForThis, arg1, arg2, ...)
+theFunction("John", "fireman");
+theFunction.apply(undefined, ["Susan", "school teacher"]);
+theFunction.call(undefined, "Claude", "mathematician");
+theFunction.call(undefined, ...["Matthew", "physicist"]); // used with the spread operator
