339743da2c785b7766fd1297272f1e55ce08016f
ArSo77 <arsobieraj@gmail.com>
2020-03-15 01:16:10 +0100

    reformat

--- react/Complete Intro to React/PureReact/src/SearchParams.js
+++ react/Complete Intro to React/PureReact/src/SearchParams.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import userDropdown from './useDropdown'
 
 const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = userDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = userDropdown("Breed", "", breeds);
 
-    const [location, setLocation]= useState("Seattle, WA");
-    return (
-        <div className="search-params">
-            <h1>{location}</h1>
-            <form>
-                <label htmlFor="location">
-                    location
-                    <input id="location"
-                           value={location}
-                           placeholder="Location"
-                           onChange={e => setLocation(e.target.value)}/>
-                </label>
-                <button>Submit</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className='search-params'>
+      <h1>{location}</h1>
+      <form>
+        <label htmlFor='location'>
+          location
+          <input id='location' value={location} placeholder='Location' onChange={(e) => setLocation(e.target.value)} />
+        </label>
+          <AnimalDropdown/>
+          <BreedDropdown/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchParams;
