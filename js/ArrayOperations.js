aca0a9dce7cc3bb80e916c0ee2dd323b80d1f93b
asobieraj <asobieraj@unitdoseone.com>
2020-04-16 21:44:42 +0200

    test2

--- js/ArrayOperations.js
+++ js/ArrayOperations.js
@@ -1,4 +1,15 @@
-let numbers = [1, 2, 3, 4]
-numbers.slice(0, 3)
-// returns [1, 2, 3]
-console.log(numbers) // returns the original array
+// let numbers = [1, 2, 3, 4, 5]
+// let resp = numbers.slice(0, 3)// returns [1, 2, 3]
+// console.log(resp, numbers) // returns the original array
+
+// numbers = [1, 2, 3, //4, 5]
+// resp = numbers.push("x") // 6
+// console.log(resp, numbers) // [1,2,3,4,5,x]
+
+numbers = ['a', 'b', 'c', 'd', 'e']
+resp = numbers.map(item => {
+    return {
+        item
+    }
+}) // 
+console.log(resp, numbers) // 
